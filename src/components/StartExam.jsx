53e1eb6dccd2fdc40f183fdc60d18b8427e33695
sahil_mujawar <mujawarmsahil@gmail.com>
2025-01-10 10:22:57 +0530

    enhanced Start exam functionality and User Interface of Add question Component

--- src/components/StartExam.jsx
+++ src/components/StartExam.jsx
@@ -4,14 +4,41 @@ import PropTypes from "prop-types";
 
 function StartExam(props){
 
-    if (!props.scheduleObj || 
+    if(
+    props.questionSet.length !== 0 &&
+    props.scheduleObj.date !== "" &&
+    props.scheduleObj.startTime !==  "" &&
+    props.scheduleObj.endTime  !== "" &&
+    props.scheduleObj.totalMarks !== 0 &&
+    props.scheduleObj.passingMarks !== 0 &&
+    new Date(`${props.scheduleObj.date}T${props.scheduleObj.startTime}`).getTime() <= new Date().getTime() && 
+    new Date(`${props.scheduleObj.date}T${props.scheduleObj.endTime}`).getTime() >= new Date().getTime())
+    {
+        return(
+            <>
+                <div className="itemContainer">
+                    <div id="card">
+                        <div className="examCard">
+                            <p>Exam date : {props.scheduleObj.date}</p>
+                            <p>Start Time : {props.scheduleObj.startTime}</p>
+                            <p>End Time : {props.scheduleObj.endTime}</p>
+                            <p>Total marks : {props.scheduleObj.totalMarks} Marks</p>
+                            <p>Passing Marks : {props.scheduleObj.passingMarks}</p>
+                            <button type="button" className="startExam"><NavLink to={`/Questions`}>Start Exam</NavLink></button>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+    else if (!props.scheduleObj || 
         props.questionSet.length === 0 ||
         props.scheduleObj.date === "" ||
         props.scheduleObj.startTime ===  "" ||
         props.scheduleObj.endTime  === "" ||
         props.scheduleObj.totalMarks === 0 ||
         props.scheduleObj.passingMarks === 0 ||
-        new Date(`${props.scheduleObj.date}T${props.scheduleObj.startTime}`).getTime() < new Date().getTime() || 
+        new Date(`${props.scheduleObj.date}T${props.scheduleObj.startTime}`).getTime() > new Date().getTime() || 
         new Date(`${props.scheduleObj.date}T${props.scheduleObj.endTime}`).getTime() < new Date().getTime()) {
         return (
             <>
@@ -24,22 +51,7 @@ function StartExam(props){
         )
     }
     
-    return(
-        <>
-            <div className="itemContainer">
-                <div id="card">
-                    <div className="examCard">
-                        <p>Exam date : {props.scheduleObj.date}</p>
-                        <p>Start Time : {props.scheduleObj.startTime}</p>
-                        <p>End Time : {props.scheduleObj.endTime}</p>
-                        <p>Total marks : {props.scheduleObj.totalMarks} Marks</p>
-                        <p>Passing Marks : {props.scheduleObj.passingMarks}</p>
-                        <button type="button" className="startExam"><NavLink to={`/Questions`}>Start Exam</NavLink></button>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+    
 }
 
 StartExam.propTypes = {
